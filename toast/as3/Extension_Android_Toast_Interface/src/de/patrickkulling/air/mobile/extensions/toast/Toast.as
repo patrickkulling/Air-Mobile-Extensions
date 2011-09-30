1eb8b25cea58e7c531ff439ff87a2f2f562bb16d
Patrick Kulling <kontakt@patrickkulling.de>
2011-09-30 20:16:11 +0200

    - adding toast messages support

---
+++ toast/as3/Extension_Android_Toast_Interface/src/de/patrickkulling/air/mobile/extensions/toast/Toast.as
@@ -0,0 +1,71 @@
+﻿/*
+ * Copyright (c) 2011 Patrick Kulling
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.patrickkulling.air.mobile.extensions.toast
+{
+	import flash.external.ExtensionContext;
+
+	public class Toast
+	{
+		private static const EXTENSION_ID : String = "de.patrickkulling.air.mobile.extensions.toast";
+
+		private static var context : ExtensionContext;
+		private static var referenceCount : int = 0;
+
+		public function Toast()
+		{
+			if (context == null)
+				initContext();
+
+			referenceCount++;
+		}
+
+		public function showToastMessage(message : String, isLongText : Boolean = false) : void
+		{
+			if (context == null)
+				return;
+
+			context.call("showToastMessage", message, isLongText);
+		}
+
+		public function dispose() : void
+		{
+			if (context == null)
+				return;
+
+			referenceCount--;
+
+			if (referenceCount < 0)
+				referenceCount = 0;
+
+			if (referenceCount == 0)
+			{
+				context.dispose();
+				context = null;
+			}
+		}
+
+		private static function initContext() : void
+		{
+			context = ExtensionContext.createExtensionContext(EXTENSION_ID, null);
+		}
+	}
+}
---
+++ toast/native_android/Extension_Android_Toast/src/de/patrickkulling/air/mobile/extensions/toast/functions/ShowToastMessage.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2011 Patrick Kulling
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.patrickkulling.air.mobile.extensions.toast.functions;
+
+import android.widget.Toast;
+
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREFunction;
+import com.adobe.fre.FREInvalidObjectException;
+import com.adobe.fre.FREObject;
+import com.adobe.fre.FRETypeMismatchException;
+import com.adobe.fre.FREWrongThreadException;
+
+public class ShowToastMessage implements FREFunction
+{
+	public FREObject call(FREContext context, FREObject[] params)
+	{
+		String message = getMessage(params[0]);
+		int toastType = getToastType(params[1]);
+
+		if (message != null)
+		{
+			Toast toast = Toast.makeText(context.getActivity(), message, toastType);
+
+			toast.show();
+		}
+
+		return null;
+	}
+
+	private String getMessage(FREObject messageObject)
+	{
+		String message = null;
+
+		try
+		{
+			message = messageObject.getAsString();
+		} catch (IllegalStateException e)
+		{
+			e.printStackTrace();
+		} catch (FRETypeMismatchException e)
+		{
+			e.printStackTrace();
+		} catch (FREInvalidObjectException e)
+		{
+			e.printStackTrace();
+		} catch (FREWrongThreadException e)
+		{
+			e.printStackTrace();
+		}
+
+		return message;
+	}
+
+	private int getToastType(FREObject toastTypeObject)
+	{
+		try
+		{
+			boolean isLongToastType = toastTypeObject.getAsBool();
+
+			if (isLongToastType == true)
+				return Toast.LENGTH_LONG;
+
+		} catch (IllegalStateException e)
+		{
+			e.printStackTrace();
+		} catch (FRETypeMismatchException e)
+		{
+			e.printStackTrace();
+		} catch (FREInvalidObjectException e)
+		{
+			e.printStackTrace();
+		} catch (FREWrongThreadException e)
+		{
+			e.printStackTrace();
+		}
+
+		return Toast.LENGTH_SHORT;
+	}
+}
